f7809c6ac4296256cda14ef3cd421f681229d18e
jvjunsay <jvjunsay@gmail.com>
2018-05-30 19:42:41 +0800

    me

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.get('/open', (req, res) => {
 async function open (_driver) {
   _driver.get('https://www.linkedin.com/uas/login?session_redirect=%2Fsales%2F&fromSignIn=true&trk=navigator')
   _driver.findElement(webdriver.By.id('session_key-login')).clear()
-  // driver.findElement(webdriver.By.id('session_key-login')).sendKeys('aaron@linkedprospect.com')
+  // _driver.findElement(webdriver.By.id('session_key-login')).sendKeys('aaron@linkedprospect.com')
   _driver.findElement(webdriver.By.id('session_key-login')).sendKeys('janjunsay@gmail.com')
   _driver.findElement(webdriver.By.id('session_password-login')).clear()
   // _driver.findElement(webdriver.By.id('session_password-login')).sendKeys('Portsmouth1')
   _driver.findElement(webdriver.By.id('session_password-login')).sendKeys('p0rkyp1g')
-  _driver.findElement(webdriver.By.id('btn-primary')).click()
-  // let verificationCode = _driver.FindElement(webdriver.By.id('verification-code'))
-  // console.log(verificationCode)
+  await _driver.findElement(webdriver.By.id('btn-primary')).click()
+  let verificationCode = _driver.findElement(webdriver.By.id('verification-code'))
+  console.log(verificationCode)
 }
 
 const PORT = process.env.PORT || 3000
